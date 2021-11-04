d6555f26cd1db52b185ac7dec4d6a1ae66e2081f
Your Name <you@example.com>
2021-11-03 21:56:13 -0300

    Adicionado um pop up com o nome dos criadores/colaboradores na main screen

--- touch_speed/lib/screens/home_menu.dart
+++ touch_speed/lib/screens/home_menu.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:touch_speed/models/configurations.dart';
 
 import '../route_generate.dart';
@@ -30,81 +32,152 @@ class _MainScreenState extends State<MainScreen> {
                 ),
               ),
               child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    RichText(
-                      textAlign: TextAlign.center,
-                      text: const TextSpan(
-                        text: "Touch Speed",
-                        style: TextStyle(
-                            color: Colors.yellow,
-                            fontSize: 60,
-                            fontFamily: "SF-Pixelate"),
+                  padding: const EdgeInsets.all(20),
+                  child: Stack(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          RichText(
+                            textAlign: TextAlign.center,
+                            text: const TextSpan(
+                              text: "Touch Speed",
+                              style: TextStyle(
+                                  color: Colors.yellow,
+                                  fontSize: 60,
+                                  fontFamily: "SF-Pixelate"),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 200,
+                          ),
+                          ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all<Color>(Colors.red),
+                              minimumSize: MaterialStateProperty.all<Size>(
+                                const Size(180.0, 60.0),
+                              ),
+                            ),
+                            onPressed: () => Navigator.of(context)
+                                .pushNamed("/touch-ball", arguments: items),
+                            child: const Text(
+                              "Touch Ball Speed",
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all<Color>(Colors.red),
+                              minimumSize: MaterialStateProperty.all<Size>(
+                                const Size(180.0, 60.0),
+                              ),
+                            ),
+                            onPressed: () =>
+                                Navigator.of(context).pushNamed("/touch-speed"),
+                            child: const Text(
+                              "Touch Speed",
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all<Color>(Colors.red),
+                              minimumSize: MaterialStateProperty.all<Size>(
+                                const Size(180.0, 60.0),
+                              ),
+                            ),
+                            onPressed: () async {
+                              final result = await Navigator.of(context)
+                                      .pushNamed("/configuration")
+                                  as ConfigurationItens;
+                              items = result;
+                              print("kkkkk => ${result.initialTime}");
+                            },
+                            child: const Text(
+                              "Configurations",
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                    const SizedBox(
-                      height: 200,
-                    ),
-                    ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.red),
-                        minimumSize: MaterialStateProperty.all<Size>(
-                          const Size(180.0, 60.0),
-                        ),
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return AlertDialog(
+                                      backgroundColor: Colors.orange,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(35.0)
+                                      ),
+                                      actionsAlignment: MainAxisAlignment.center,
+                                      title: const Center(child: Text('Criadores')),
+                                      titleTextStyle: const TextStyle(
+                                        fontSize: 35.0,
+                                        fontWeight: FontWeight.bold,
+                                        fontFamily: "SF-Pixelate",
+                                        color: Colors.deepPurple
+                                      ),
+                                      content:
+                                      Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: const [
+                                          Padding(padding: EdgeInsets.only(top: 15.0)),
+                                          Text(
+                                            '- Pablo Raphael',
+                                            style: TextStyle(
+                                              fontSize: 21.5
+                                            ),
+                                          ),
+                                          Text(
+                                            '- Adriel de Oliveira',
+                                            style: TextStyle(
+                                                fontSize: 21.5
+                                            ),
+                                          ),
+                                          Text(
+                                            '- Thamily Lopes',
+                                            style: TextStyle(
+                                                fontSize: 21.5
+                                            ),
+                                          ),
+                                          Text(
+                                            '- Isaac Santana',
+                                            style: TextStyle(
+                                                fontSize: 21.5
+                                            ),
+                                          ),
+                                          Text(
+                                            '- Gustavo Anjos',
+                                            style: TextStyle(
+                                                fontSize: 21.5
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    );
+                                  },
+                              );
+                            },
+                            iconSize: 50.0,
+                            icon: const Icon(Icons.supervisor_account_outlined),
+                          )
+                        ],
                       ),
-                      onPressed: () => Navigator.of(context)
-                          .pushNamed("/touch-ball", arguments: items),
-                      child: const Text(
-                        "Touch Ball Speed",
-                        style: TextStyle(fontSize: 20.0),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.red),
-                        minimumSize: MaterialStateProperty.all<Size>(
-                          const Size(180.0, 60.0),
-                        ),
-                      ),
-                      onPressed: () =>
-                          Navigator.of(context).pushNamed("/touch-speed"),
-                      child: const Text(
-                        "Touch Speed",
-                        style: TextStyle(fontSize: 20.0),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.red),
-                        minimumSize: MaterialStateProperty.all<Size>(
-                          const Size(180.0, 60.0),
-                        ),
-                      ),
-                      onPressed: () async {
-                        final result = await Navigator.of(context)
-                            .pushNamed("/configuration") as ConfigurationItens;
-                        items = result;
-                        print("kkkkk => ${result.initialTime}");
-                      },
-                      child: const Text(
-                        "Configurations",
-                        style: TextStyle(fontSize: 20.0),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+                    ],
+                  )),
             ),
           ),
         ),
